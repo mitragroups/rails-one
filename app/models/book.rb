910d7a7810fb86d6063923dc5aa3a78a340db42a
mitragroups <miftahmuhajir@gmail.com>
2021-10-30 01:34:09 +0700

    Add Book Data

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,10 @@
 class Book < ApplicationRecord
+    def self.expensive
+        where('price > 100000')
+    end
+    
+    def self.price_equal_more(price)
+        where('price >= ?', price)
+    end
+    
 end
